e514881bab8c1b506ff628506252e083e109976d
luis.aragon <luis.aragon@grupossi.com>
2018-01-02 17:29:54 -0500

    test

--- SSI7X/main.py
+++ SSI7X/main.py
@@ -13,7 +13,7 @@ images = Blueprint('images', __name__, static_folder='/img')
 app = Flask(__name__)
 
 #To Do:Verificar la seguridad del cors  
-# commit oscar   
+# commit luis
 CORS(app)
 
 api = Api( auth, prefix="/login")
