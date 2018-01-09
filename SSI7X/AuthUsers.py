bf7bcc6bb4e5cd32d8df1c3b2aa6516983a08010
robin valencia <robin.w.valencia@gmail.com>
2018-01-09 14:44:17 -0500

    cambios en el retorno de api

--- SSI7X/AuthUsers.py
+++ SSI7X/AuthUsers.py
@@ -7,11 +7,11 @@ from SSI7X.Static.ConnectDB import ConnectDB  # @UnresolvedImport
 from SSI7X.Static.Utils import Utils  # @UnresolvedImport
 import SSI7X.Static.errors as errors  # @UnresolvedImport
 import SSI7X.Static.config as conf  # @UnresolvedImport
-import SSI7X.Static.config_DB as confDB  # @UnresolvedImport
+import SSI7X.Static.config_DB as dbConf
 
 class UsuarioAcceso(Form):
-    usro = StringField('Nombre de usuario',[validators.DataRequired(message=errors.ERR_NO_02)])
-    cntrsna = StringField('Cntrasenna de usuario',[validators.DataRequired(message=errors.ERR_NO_03)])
+    username = StringField('Nombre de usuario',[validators.DataRequired(message=errors.ERR_NO_02)])
+    password = StringField('Cntrasenna de usuario',[validators.DataRequired(message=errors.ERR_NO_03)])
     
 class UsroCmbioCntrsna(Form):
     cntrsna = StringField('Cntrasenna de usuario',[validators.DataRequired(message=errors.ERR_NO_03)])
@@ -23,22 +23,24 @@ class AutenticacionUsuarios(Resource):
         u = UsuarioAcceso(request.form)
         utils = Utils()
         if not u.validate():
-            return utils.nice_json({"status":"Error","error":u.errors,"user":"null"})
-        
+            #return utils.nice_json({"status":"Error","error":u.errors,"user":"null"})
+            return utils.nice_json({"error":u.errors})
         c = ConnectDB()
-        md5= hashlib.md5(request.form['cntrsna'].encode('utf-8')).hexdigest() 
-        cursor = c.querySelect(confDB.DB_SHMA +'.tblogins', 'lgn,cntrsna', "lgn='"+ request.form['usro']+ "' and  cntrsna='"+md5+"'")
+        md5= hashlib.md5(request.form['password'].encode('utf-8')).hexdigest() 
+        cursor = c.querySelect(dbConf.DB_SHMA +'.tblogins', 'lgn,cntrsna', "lgn='"+ request.form['username']+ "' and  cntrsna='"+md5+"'")
         data=[]
         if cursor :
             session['logged_in'] = True
-            token = os.urandom(conf.SS_TKN_SIZE)
+            token = os.urandom(24)
             session['token'] = str(token).encode(encoding='utf_8', errors='strict')
             data.append({"logged_in":session['logged_in'],"token":token})
-            return utils.nice_json({"status":"OK","error":"null","session":str(data)})
+            #return utils.nice_json({"status":"OK","error":"null","access_token ":str(token),"session":str(data)})
+            return utils.nice_json({"access_token":str(token)})
         else:
             session['logged_in'] = False
             data.append({"logged_in":session['logged_in'],"token":''})
-            return utils.nice_json({"status":"Error","error":errors.ERR_NO_01,"session":str(data)})
+            #return utils.nice_json({"status":"Error","error":errors.ERR_NO_01,"session":str(data)})
+            return utils.nice_json({"error":errors.ERR_NO_01})
 class CmboCntrsna(Resource):
     def post(self):
         u = UsroCmbioCntrsna(request.form)
