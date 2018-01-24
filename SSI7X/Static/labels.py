cfad33cd7b370903b968dea5fd383f7ae5762e26
Luis Aragon <luis.aragon@grupossi.com>
2018-01-24 11:02:18 -0500

    se sube nuevamente el labels

--- SSI7X/Static/labels.py
+++ SSI7X/Static/labels.py
@@ -3,3 +3,5 @@
 lbl_nmbr_usrs = 'Nombre de usuario'
 lbl_cntrsna_usrs = 'Contraseña de usuario'
 lbl_nva_cntrsna = 'Nueva Contraseña'
+lbl_cdgo_prfl = 'Codigo Perfil'
+lbl_dscrpcn_prfl =  'Descripcion Perfil'
